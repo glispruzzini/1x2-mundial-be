b6558314d39658f3fea78f4f63e3f7804e2e57a5
Fabio Bellini <fabio.bellini@crispybacon.it>
2018-06-14 23:29:46 +0200

    Added firebase-auth logics

--- src/services/firebaseAdmin.ts
+++ src/services/firebaseAdmin.ts
@@ -1,8 +1,21 @@
+import { Response, NextFunction } from "express";
+import { AuthoredRequest } from "../interfaces/customExpress";
 import * as admin from 'firebase-admin';
 
-const test = require("../../secret-secretante.json");
+const credentials = require("../../secret-secretante.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(test)
+    credential: admin.credential.cert(credentials)
 });
 
+export function firebaseAuthMiddleware(req: AuthoredRequest, res: Response, next: NextFunction) {
+    return admin
+        .auth()
+        .verifyIdToken(req.header('Authorization'))
+        .then(function(decodedToken) {
+            req.user = decodedToken;
+            next();
+        }).catch(function(error) {
+            res.status(401);
+        });
+}
