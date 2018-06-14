f5631aac35bfce9e8f68bed357a28ffaf1e1bc62
Fabio Bellini <fabio.bellini@crispybacon.it>
2018-06-15 00:44:55 +0200

    Added routing for matches

--- src/securityRouter.ts
+++ src/securityRouter.ts
@@ -1,5 +1,8 @@
 import * as express from "express";
+import matchRouting from "./routings/match.routing";
 
 const router = express.Router();
+
+router.use("/matches", matchRouting);
 
 export default router;
